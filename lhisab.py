d66fc3f6a5d3c79f7d61bed79d0646871c8c648f
salah eddine <salah.mez@fella.ga>
2017-06-24 10:20:24 -0100

    calculatins arai of rect

---
+++ lhisab.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#Filename; helloworld.py
+def area_rect():
+ lenght=input("plaes enter your lenght : ");
+ wight =input("plaes enter your wight  : ");	
+ area1  = wight * lenght
+ print "the area  is ",area1 ;
+
+def area_cercle():
+	radius=input("plaes enter your radius :");
